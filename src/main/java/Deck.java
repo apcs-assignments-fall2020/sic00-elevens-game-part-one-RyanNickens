f337d1e6f8bad80cc21deda785abe18d680d86d1
RyanNickens <22rnickens@athenian.org>
2021-01-10 19:40:16 +0000

    fully functioning code

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -30,7 +30,14 @@ public class Deck {
 	 * @param values is an array containing all of the card point values.
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
-		// YOUR CODE HERE
+        cards = new ArrayList<Card>();
+        for (int i = 0; i < ranks.length; i++) {
+            for (int j = 0; j < suits.length; j++) {
+                cards.add(new Card(ranks[i], suits[j], values[i]));
+            }
+        }
+        size = cards.size();
+        
 	}
 
 	/**
@@ -38,8 +45,8 @@ public class Deck {
 	 * @return true if this deck is empty, false otherwise.
 	 */
 	public boolean isEmpty() {
-        // YOUR CODE HERE
-        return false;
+        
+        return size == 0;
 	}
 
 	/**
@@ -47,8 +54,8 @@ public class Deck {
 	 * @return the number of undealt cards in this deck.
 	 */
 	public int getSize() {
-        // YOUR CODE HERE
-        return -1;
+        
+        return size;
 	}
 
 	/**
@@ -58,7 +65,13 @@ public class Deck {
 	 */
 	public Card deal() {
 		// YOUR CODE HERE
-        return null;
+        if (isEmpty() == true) {
+			return null;
+        }
+        
+		size--;
+		Card x = cards.get(size);
+		return x;
 	}
 
 	/**
--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -4,6 +4,16 @@
 public class Tester {
 
 	public static void main(String[] args) {
-		// YOUR TESTING CODE HERE
+
+		String[] ranks = {"Ace", "2", "3"};
+        String[] suits = {"Clubs", "Hearts"};
+        int[] values = {1, 2, 3};
+        Deck d1 = new Deck(ranks, suits, values);
+        //System.out.println(d1);    
+        
+        Card c1 = d1.deal();
+        System.out.println(c1);
+        System.out.println();
+        System.out.println(d1);
 	}
 }
